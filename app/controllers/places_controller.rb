e1ec3be340b3c67fe794630e37f3ed12249e5d8c
dikssha dinesh <dikssha.dinesh@kellogg.northwestern.edu>
2023-06-06 03:14:48 +0000

    commit2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,14 +1,15 @@
 class PlacesController < ApplicationController
 
   def index
-    if @current_user
     @places = Place.all
-    end
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
     @posts = Post.where({ "place_id" => @place["id"] })
+    if @current_user
+      @posts = Post.where({ "user_id" => @current_user["id"]})
+    end
   end
 
   def new
@@ -18,6 +19,7 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new
     @place["name"] = params["place"]["name"]
+    @place["user"] = params["place"]["name"]
     @place.save
     redirect_to "/places"
   end
